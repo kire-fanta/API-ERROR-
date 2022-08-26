22766e134cea72efc3348a6e9b8c5bbc0ecf21ef
M’ba Fatoumata kire <mfkire@odc.local>
2022-08-26 15:21:18 +0000

    1 depot

---
+++ ErrorApi/src/main/java/com/api/ErrorApi/ErrorApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.ErrorApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class                                                                                                                                                                                                                                                                                                                                                      ErrorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErrorApiApplication.class, args);
+	}
+
+}
---
+++ ErrorApi/src/test/java/com/api/ErrorApi/ErrorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.ErrorApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErrorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
